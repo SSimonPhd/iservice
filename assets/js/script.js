e4ea478d1fd74c119375314f9ade4bc93cf1eaeb
Scott Simon <simon_phd@protonmail.com>
2022-09-11 00:52:21 -0500

    Added Google map and styles to demo

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,21 @@
 // Event listeners
 
 // Local Storage
+
+// Google map
+function initMap() {
+  // Location
+  const austin = { lat: 30.266666, lng: -97.733330 };
+  // The map, zoomed at Austin
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: austin,
+  });
+  // The marker, positioned at Austin
+  const marker = new google.maps.Marker({
+    position: austin,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
