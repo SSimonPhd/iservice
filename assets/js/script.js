920e2ec97791aca083b013a01585c90911ff77eb
Scott Simon <simon_phd@protonmail.com>
2022-09-11 04:19:23 -0500

    Added geolocation and google map apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,92 @@
 // Local Storage
 
 // Google map
+// function initMap() {
+//   // Location
+//   const austin = { lat: 30.266666, lng: -97.733330 };
+//   // The map, zoomed at Austin
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 10,
+//     center: austin,
+//   });
+//   // The marker, positioned at Austin
+//   const marker = new google.maps.Marker({
+//     position: austin,
+//     map: map,
+//   });
+// }
+
+// window.initMap = initMap;
+
+let map, infoWindow;
+
 function initMap() {
-  // Location
-  const austin = { lat: 30.266666, lng: -97.733330 };
-  // The map, zoomed at Austin
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 30.266666, lng: -97.733330},
     zoom: 10,
-    center: austin,
   });
-  // The marker, positioned at Austin
-  const marker = new google.maps.Marker({
-    position: austin,
-    map: map,
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
   });
 }
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
 window.initMap = initMap;
+
+
+
+// IP geolocator api
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+var requestOptions = {
+    method: "get",
+    headers: myHeaders,
+    redirect: "follow",
+};
+
+fetch("https://v1.nocodeapi.com/simonphd/lookup/doTeAmBZbLzsExUV", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+		console.log = function(message) {
+			document.getElementById('result').innerHTML = message;
+	};
+	console.log('');
